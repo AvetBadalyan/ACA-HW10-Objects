cd34274fe71e1923dbdf1d77a1c294e73f5cb0b2
AvetBadalyan <avetbadalyan@gmail.com>
2021-12-14 19:48:13 +0400

    feat: exemple of Object destructuring

--- 01.ObjectDistructure.js
+++ 01.ObjectDistructure.js
@@ -1,4 +1,14 @@
 // Object distructure
+
+/*
 let person = { name: "Sarah", country: "Nigeria", job: "Developer" };
 const { name, country, job } = person;
 console.log(name);
+*/
+
+// Get name, country and job using destructuring
+let person = { name: "Sarah", country: "Armenia", job: "Developer" };
+const { name, country, job } = person;
+console.log(name); // "Sarah"
+console.log(country); // "Nigeria"
+console.log(job); // "Developer"
